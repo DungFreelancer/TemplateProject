0c6bc5af4d3955f8b6d93122dced913c72494693
Huy Tran <samchenjia@gmail.com>
2017-10-01 11:56:16 +0700

    + Add: get document directory & show alert functions.

--- TemplateProject/Helpers/Commons.swift
+++ TemplateProject/Helpers/Commons.swift
@@ -19,4 +19,38 @@ class Commons {
         USER_DEFAULT.removePersistentDomain(forName: appDomain)
     }
     
+    func applicationDocumentDirectoryString() -> String {
+        let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        let documentDirectory = paths[0]
+        return documentDirectory
+    }
+    
+    func showAlertOnViewController(_ viewController: UIViewController,
+                                   withTitle title: String?,
+                                   andMessage message: String,
+                                   andMainButton mainButton: String?,
+                                   completionMain mainComplete: ((UIAlertAction)->())?,
+                                   andOtherButton otherButton: String?,
+                                   completionOther otherComplete: ((UIAlertAction)->())?) {
+        let alert: UIAlertController
+        
+        if title == nil || title == "" {
+            alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        } else {
+            alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        }
+        
+        if let mainButton = mainButton {
+            let mainAction = UIAlertAction(title: mainButton, style: .default, handler: mainComplete)
+            alert.addAction(mainAction)
+        }
+        
+        if let otherButton = otherButton {
+            let otherAction = UIAlertAction(title: otherButton, style: .cancel, handler: otherComplete)
+            alert.addAction(otherAction)
+        }
+        
+        viewController.present(alert, animated: true, completion: nil)
+    }
+    
 }
