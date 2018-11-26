c4bac35be05a2005d3335d099ee12c2ec51dd9c7
DungDo <dungdo11051991@gmail.com>
2018-11-26 16:07:58 +0700

    Add: init UIColor with hex. (Update)

--- TemplateProject/Helpers/Extension/UIColorEx.swift
+++ TemplateProject/Helpers/Extension/UIColorEx.swift
@@ -12,12 +12,12 @@ extension UIColor {
     
     // Ex: UIColor(r: 95, g: 100, b: 105)
     convenience init(r: Int, g: Int, b: Int) {
-        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
+        self.init(red: CGFloat(r)/255, green: CGFloat(g)/255, blue: CGFloat(b)/255, alpha: 1.0)
     }
     
     // Ex: UIColor(hex: 0x5fc7dc)
     convenience init(hex: Int) {
-        self.init(r: (hex >> 16) & 0xff, g: (hex >> 8) & 0xff, b: hex 0xff)
+        self.init(r: (hex >> 16) & 0xff, g: (hex >> 8) & 0xff, b: 0xff)
     }
     
 }
