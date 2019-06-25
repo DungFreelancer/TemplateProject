4afd1e8a19af8490ade5c2c898b62cc7fcb68c1c
DungDo <dungdo11051991@gmail.com>
2019-06-25 11:53:27 +0700

    !Update: some commons class.

--- TemplateProject/Common/Extension/UIViewEx.swift
+++ TemplateProject/Common/Extension/UIViewEx.swift
@@ -10,20 +10,40 @@ import UIKit
 
 extension UIView {
     
-    var width: CGFloat {
-        return self.frame.width;
-    }
-    
-    var height: CGFloat {
-        return self.frame.height;
-    }
-    
     var x: CGFloat {
-        return self.frame.origin.x
+        get {
+            return self.frame.origin.x
+        }
+        set {
+            self.frame = CGRect(x: newValue, y: self.frame.origin.y, width: self.frame.size.width, height: self.frame.size.height)
+        }
     }
     
     var y: CGFloat {
-        return self.frame.origin.y
+        get {
+            return self.frame.origin.y
+        }
+        set {
+            self.frame = CGRect(x: self.frame.origin.x, y: newValue, width: self.frame.size.width, height: self.frame.size.height)
+        }
+    }
+    
+    var width: CGFloat {
+        get {
+            return self.frame.size.width
+        }
+        set {
+            self.frame = CGRect(x: self.frame.origin.x, y: self.frame.origin.y, width: newValue, height: self.frame.size.height)
+        }
+    }
+    
+    var height: CGFloat {
+        get {
+            return self.frame.height
+        }
+        set {
+            self.frame = CGRect(x: self.frame.origin.x, y: self.frame.origin.y, width: self.frame.size.width, height: newValue)
+        }
     }
     
     func setRoundedCornersFull() {
