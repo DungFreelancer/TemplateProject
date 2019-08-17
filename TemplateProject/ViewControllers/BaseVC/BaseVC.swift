dcb4ea2cc491c2039ac8570ea58bddc36907dbef
DungDo <dungdo11051991@gmail.com>
2019-08-17 09:56:51 +0700

    !Update: BaseVC class.

--- TemplateProject/ViewControllers/BaseVC/BaseVC.swift
+++ TemplateProject/ViewControllers/BaseVC/BaseVC.swift
@@ -9,11 +9,63 @@
 import UIKit
 
 class BaseVC: UIViewController {
-
+    
+    enum SwitchAnimation {
+        case top, bottom, fade
+    }
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.settupViews()
     }
     
+    // MARK: - private function
+    private func settupViews() {
+        self.navigationController?.isNavigationBarHidden = true
+        
+        let tapCloseKeyboard = UITapGestureRecognizer(target: self, action: #selector(closeKeyboard))
+        self.view.addGestureRecognizer(tapCloseKeyboard)
+        tapCloseKeyboard.cancelsTouchesInView = false
+    }
+    
+    @objc func closeKeyboard() {
+        self.view.endEditing(true)
+    }
+    
+    func backToRoot(animation: SwitchAnimation = .fade) {
+        self.switchToAnimation(animation)
+        self.navigationController?.popToRootViewController(animated: false)
+    }
+    
+    func show(_ vc: UIViewController, animation: SwitchAnimation) {
+        self.switchToAnimation(animation)
+        self.navigationController?.pushViewController(vc, animated: false)
+    }
+    
+    func hidden(animation: SwitchAnimation = .fade) {
+        self.switchToAnimation(animation)
+        self.navigationController?.popViewController(animated: false)
+    }
+    
+    private func switchToAnimation(_ animation: SwitchAnimation) {
+        let transition = CATransition()
+        transition.duration = 1
+        transition.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+        
+        switch animation {
+        case .top:
+            transition.type = .push
+            transition.subtype = .fromTop
+        case .bottom:
+            transition.type = .push
+            transition.subtype = .fromBottom
+        case .fade:
+            transition.type = .fade
+        }
+        
+        self.navigationController?.view.layer.removeAnimation(forKey: "ShowAnimation")
+        self.navigationController?.view.layer.add(transition, forKey: "ShowAnimation")
+    }
 }
-
