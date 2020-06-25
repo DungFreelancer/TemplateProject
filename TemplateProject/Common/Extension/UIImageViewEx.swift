cabda141066fa10e5298ec319dafc1ca419402e9
DungDo <dung11051991@gmail.com>
2020-06-25 16:07:39 +0700

    !Update: libs

--- TemplateProject/Common/Extension/UIImageViewEx.swift
+++ TemplateProject/Common/Extension/UIImageViewEx.swift
@@ -10,15 +10,32 @@ import Kingfisher
 
 extension UIImageView {
     
-    func download(from url: String, complete:((Bool?, String?)->())? = nil) {
+    func download(from url: String, complete:((Bool, String?)->())? = nil) {
+        // Set default Ram cache size to 300 MB.
+//        ImageCache.default.memoryStorage.config.totalCostLimit = 300 * 1024 * 1024
+        // Set default Disk cache size to 1 GB.
+//        ImageCache.default.diskStorage.config.sizeLimit = 1000 * 1024 * 1024
+        
         self.kf.indicatorType = .activity
-        self.kf.setImage(with: URL(string: url), placeholder: nil, options: nil, progressBlock: nil) { (result) in
+        
+        self.kf.cancelDownloadTask()
+        self.kf.setImage(with: URL(string: url),
+                         placeholder: nil,
+                         options: [
+                             .processor(DownsamplingImageProcessor(size: self.frame.size)),
+                             .scaleFactor(UIScreen.main.scale),
+                             .cacheOriginalImage
+                         ],
+                         progressBlock: nil) { (result) in
             if complete != nil {
                 switch result {
                 case .success(_):
                     complete!(true, nil)
                 case .failure(let error):
-                    complete!(nil, error.localizedDescription)
+                    if !error.isTaskCancelled {
+                        Log.e(error.errorDescription ?? error.localizedDescription)
+                    }
+                    complete?(false, error.errorDescription)
                 }
             }
         }
