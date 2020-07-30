d525c7b1264ece23181d5f8aeefdd44f9719228f
DungDo <dung11051991@gmail.com>
2020-07-30 14:08:45 +0700

    -Remove: some extension

--- TemplateProject/Common/Extension/UIButtonEx.swift
+++ TemplateProject/Common/Extension/UIButtonEx.swift
@@ -10,30 +10,6 @@ import UIKit
 
 extension UIButton {
     
-    @IBInspectable
-    var charSpacing: CGFloat {
-        set {
-            let attributedString: NSMutableAttributedString!
-            if let currentAttrString = self.titleLabel?.attributedText {
-                attributedString = NSMutableAttributedString(attributedString: currentAttrString)
-            } else {
-                attributedString = NSMutableAttributedString(string: self.titleLabel?.text ?? "")
-                self.titleLabel?.text = nil
-            }
-            attributedString.addAttribute(NSAttributedString.Key.kern,
-                                          value: newValue,
-                                          range: NSRange(location: 0, length: attributedString.length))
-            self.titleLabel?.attributedText = attributedString
-        }
-        get {
-            if let currentCharacterSpace = self.titleLabel?.attributedText?.attribute(NSAttributedString.Key.kern, at: 0, effectiveRange: .none) as? CGFloat {
-                return currentCharacterSpace
-            } else {
-                return 0
-            }
-        }
-    }
-    
     override open func sendAction(_ action: Selector, to target: Any?, for event: UIEvent?) {
         super.sendAction(action, to: target, for: event)
         
